d0a09c47db73108c70f6bd3b5e4e0f933bb759d1
Alex <alexvingg@gmail.com>
2023-02-10 18:07:22 +0000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name='multiply',
-    version='1.0.0',
-    description='test lib multiply project',
+    version='1.0',
+    description='A tutorial for creating pip packages.',
+    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    author='Alex',
+    author_email='alexvingg@gmail.com',
     packages=['multiply',]
 )
